3130e207186854a45c0371694cca3c060f7b73d7
hnxb <huynhninhbinh123@gmail.com>
2021-08-25 19:38:16 +0700

    feat(app): add swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { LanguagesModule } from './languages/languages.module';
 import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
   imports: [CoreModule, LanguagesModule, QuizzesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder().setTitle('js-question unofficial api').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  app.enableCors();
+  await app.listen(port);
 }
-bootstrap();
+void bootstrap();
