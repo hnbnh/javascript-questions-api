7c900f4b18dcb3487e17267d7c25e74291194431
hnxb <huynhninhbinh123@gmail.com>
2021-08-26 14:03:30 +0700

    chore(quizzes): update api description

--- src/quizzes/quizzes.controller.ts
+++ src/quizzes/quizzes.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param, ParseIntPipe, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { QuizzesService } from './quizzes.service';
 import { FindQuizDto } from './dto';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 import { ApiImplicitQuery } from '@nestjs/swagger/dist/decorators/api-implicit-query.decorator';
 
 @ApiTags('quizzes')
@@ -12,7 +12,7 @@ export class QuizzesController {
   @Get(':languageId')
   @UsePipes(new ValidationPipe({ transform: true }))
   @ApiOkResponse({ description: 'Get all quizzes by languageId' })
-  @ApiImplicitQuery({ name: 'languageId', required: false })
+  @ApiParam({ name: 'languageId' })
   @ApiImplicitQuery({ name: 'limit', required: false })
   @ApiImplicitQuery({ name: 'offset', required: false })
   find(@Param('languageId', new ParseIntPipe()) id: number, @Query() dto: FindQuizDto) {
@@ -22,6 +22,7 @@ export class QuizzesController {
   @Get(':languageId/random')
   @UsePipes(new ValidationPipe())
   @ApiOkResponse({ description: 'Get a random quiz by languageId' })
+  @ApiParam({ name: 'languageId' })
   findRandom(@Param('languageId', new ParseIntPipe()) id: number) {
     return this.quizzesService.findRandom(id);
   }
